2993aefc49085c22da4e627a9722205e212c605b
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-10 13:17:15 +0530

    service handled for fetching weather data

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:hive/hive.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_application/models/weather_model.dart';
+import 'package:weather_application/secrets/api_key.dart';
+
+class WeatherService {
+
+ Future<WeatherModel?> fetchWeather(String city) async {
+  final box = Hive.box('weatherCache');
+  final cacheData = box.get(city);
+
+  if (cacheData != null){
+    print("Loading weather from cache for $city");
+    return WeatherModel.fromJson(Map<String, dynamic>.from(cacheData));
+  }
+
+  final url = "https://api.openweathermap.org/data/2.5/weather?q=$city,uk&APPID=$apiKey";
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200){
+    final jsonData = jsonDecode(response.body);
+    box.put(city, jsonData); // Complete data would be saved rawly for future ease
+    return WeatherModel.fromJson(jsonData);
+  }
+  else {
+      print('Failed to load weather data: ${response.statusCode}');
+      return null;
+    }
+  }
+}
